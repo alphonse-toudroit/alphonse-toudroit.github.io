4224529a5ddaddef8f1650abb91e173dfcfb21ba
alphonse-toudroit <alphonse.toudroit@protonmail.com>
2020-09-12 14:28:07 +0200

    Update app path

--- igp-charts/app/configuration.js
+++ igp-charts/app/configuration.js
@@ -2,6 +2,6 @@
 
     angular
         .module('configuration', [])
-        .constant('DATA_DIR', 'https://alphonse-toudroit.github.io/data');
+        .constant('DATA_DIR', 'https://alphonse-toudroit.github.io/igp-charts/data');
 
 })();
